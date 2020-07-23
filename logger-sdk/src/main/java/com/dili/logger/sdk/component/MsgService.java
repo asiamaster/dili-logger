972c78ea7e33efecae394f741108f689b152d88b
wangmi <87730066>
2020-07-23 16:34:37 +0800

    解决长时间无法恢复的Connection中断

--- logger-sdk/src/main/java/com/dili/logger/sdk/component/MsgService.java
+++ logger-sdk/src/main/java/com/dili/logger/sdk/component/MsgService.java
@@ -6,6 +6,7 @@ import com.dili.logger.sdk.domain.BusinessLog;
 import com.dili.logger.sdk.domain.ExceptionLog;
 import com.dili.logger.sdk.dto.CorrelationDataExt;
 import com.dili.logger.sdk.glossary.LoggerConstant;
+import org.springframework.amqp.AmqpConnectException;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageBuilder;
 import org.springframework.amqp.core.MessageProperties;
@@ -81,6 +82,13 @@ public class MsgService {
         correlationData.setMessage(message);
         correlationData.setExchange(LoggerConstant.MQ_LOGGER_TOPIC_EXCHANGE);
         correlationData.setRoutingKey(routingKey);
-        this.rabbitTemplate.convertAndSend(LoggerConstant.MQ_LOGGER_TOPIC_EXCHANGE, routingKey, message, correlationData);
+        //有可能长时间retry之后依然不能恢复Connection，如rabbitmq挂掉的情况，不能一直retry下去阻塞接口调用
+        //这种情况是没有confirm的，因为消息都没有发出去。所以处理就更简单了
+        //retry失败或者没有retry机制都会抛出AmqpConnectException，catch之后将消息保存起来即可
+        try{
+            this.rabbitTemplate.convertAndSend(LoggerConstant.MQ_LOGGER_TOPIC_EXCHANGE, routingKey, message, correlationData);
+        }catch (AmqpConnectException e) {
+            SendFailedMessageHolder.add(correlationData);
+        }
     }
 }
