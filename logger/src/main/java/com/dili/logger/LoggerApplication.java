dc6a6415096cb763f535ae3b961832b518c09b6a
岳洪波 <yuehongbo@diligrp.com>
2020-08-14 14:24:22 +0800

    uap升级到2.8.2引发的log注解的问题解决

--- logger/src/main/java/com/dili/logger/LoggerApplication.java
+++ logger/src/main/java/com/dili/logger/LoggerApplication.java
@@ -3,14 +3,16 @@ package com.dili.logger;
 import com.dili.ss.retrofitful.annotation.RestfulScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author yuehongbo
  */
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.dili.ss", "com.dili.logger", "com.dili.uap.sdk"})
+@ComponentScan(basePackages = {"com.dili.*"})
 @RestfulScan({"com.dili.uap.sdk.rpc"})
+@EnableFeignClients(basePackages = {"com.dili.*"})
 public class LoggerApplication {
 
     public static void main(String[] args) {
