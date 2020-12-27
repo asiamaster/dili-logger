846731050e0545e83655a7008eae27425e41ebdb
岳洪波 <yuehongbo@diligrp.com>
2020-12-27 18:27:43 +0800

    工具类方法优化

--- logger-sdk/src/main/java/com/dili/logger/sdk/util/LoggerUtil.java
+++ logger-sdk/src/main/java/com/dili/logger/sdk/util/LoggerUtil.java
@@ -17,21 +17,21 @@ public class LoggerUtil {
      * @param businessCode 业务编号
      * @param operatorId 操作人ID
      * @param operatorName 操作人姓名
-     * @param operationType 操作类型
      * @param marketId 所属市场
      * @param content 日志内容
      * @param notes 日志备注
+     * @param operationType 操作类型
      * @param businessType 业务类型
      */
-    public static void buildBusinessLoggerContext(Long businessId, String businessCode, Long operatorId, String operatorName, String operationType, Long marketId, String content, String notes, String businessType) {
+    public static void buildBusinessLoggerContext(Long businessId, String businessCode, Long operatorId, String operatorName, Long marketId, String content, String notes, String operationType, String businessType) {
         LoggerContext.put(LoggerConstant.LOG_BUSINESS_CODE_KEY, businessCode);
         LoggerContext.put(LoggerConstant.LOG_BUSINESS_ID_KEY, businessId);
         LoggerContext.put(LoggerConstant.LOG_OPERATOR_ID_KEY, operatorId);
         LoggerContext.put(LoggerConstant.LOG_OPERATOR_NAME_KEY, operatorName);
-        LoggerContext.put(LoggerConstant.LOG_OPERATION_TYPE_KEY, operationType);
         LoggerContext.put(LoggerConstant.LOG_MARKET_ID_KEY, marketId);
         LoggerContext.put(LoggerConstant.LOG_CONTENT_KEY, content);
         LoggerContext.put(LoggerConstant.LOG_NOTES_KEY, notes);
+        LoggerContext.put(LoggerConstant.LOG_OPERATION_TYPE_KEY, operationType);
         LoggerContext.put(LoggerConstant.LOG_BUSINESS_TYPE, businessType);
     }
 
@@ -41,13 +41,13 @@ public class LoggerUtil {
      * @param businessCode 业务编号
      * @param operatorId 操作人ID
      * @param operatorName 操作人姓名
-     * @param operationType 操作类型
      * @param marketId 所属市场
      * @param content 日志内容
      * @param notes 日志备注
+     * @param operationType 操作类型
      */
-    public static void buildBusinessLoggerContext(Long businessId, String businessCode, Long operatorId, String operatorName, String operationType, Long marketId, String content, String notes) {
-        buildBusinessLoggerContext(businessId, businessCode, operatorId, operatorName, operationType, marketId, content, notes, null);
+    public static void buildBusinessLoggerContext(Long businessId, String businessCode, Long operatorId, String operatorName, Long marketId, String content, String notes, String operationType) {
+        buildBusinessLoggerContext(businessId, businessCode, operatorId, operatorName, marketId, content, notes, operationType, null);
     }
 
     /**
@@ -56,12 +56,11 @@ public class LoggerUtil {
      * @param businessCode 业务编号
      * @param operatorId 操作人ID
      * @param operatorName 操作人姓名
-     * @param operationType 操作类型
      * @param marketId 所属市场
      * @param content 日志内容
      */
-    public static void buildBusinessLoggerContext(Long businessId, String businessCode, Long operatorId, String operatorName, String operationType, Long marketId, String content) {
-        buildBusinessLoggerContext(businessId, businessCode, operatorId, operatorName, operationType, marketId, content, null, null);
+    public static void buildBusinessLoggerContext(Long businessId, String businessCode, Long operatorId, String operatorName, Long marketId, String content) {
+        buildBusinessLoggerContext(businessId, businessCode, operatorId, operatorName, marketId, content, null, null, null);
     }
 
     /**
@@ -70,10 +69,9 @@ public class LoggerUtil {
      * @param businessCode  业务编号
      * @param operatorId    操作人ID
      * @param operatorName  操作人姓名
-     * @param operationType 操作类型
      * @param marketId      所属市场
      */
-    public static void buildBusinessLoggerContext(Long businessId, String businessCode, Long operatorId, String operatorName, String operationType, Long marketId) {
-        buildBusinessLoggerContext(businessId, businessCode, operatorId, operatorName, operationType, marketId, null, null, null);
+    public static void buildBusinessLoggerContext(Long businessId, String businessCode, Long operatorId, String operatorName, Long marketId) {
+        buildBusinessLoggerContext(businessId, businessCode, operatorId, operatorName, marketId, null, null, null, null);
     }
 }
